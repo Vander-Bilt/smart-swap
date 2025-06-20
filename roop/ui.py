a3c8f10c5b6a7cdf2557b7cb2d9c05e9a7c8ed6a
stephen <6983299@gmail.com>
2025-06-20 10:40:42 +0800

    flag for reg notion

--- roop/ui.py
+++ roop/ui.py
@@ -342,8 +342,10 @@ def run():
             app, local_url, share_url = ui.queue().launch(inbrowser=True, server_name=server_name, server_port=server_port, share=roop.globals.CFG.server_share, ssl_verify=ssl_verify, prevent_thread_lock=True, show_error=True)
             print("Can see me?")
             print(f"Got share url: {share_url}")
-            notion.delete_all_records()
-            notion.add_record_to_notion_database(share_url)
+            if roop.globals.CFG.reg_notion:
+                print("Register notion")
+                notion.delete_all_records()
+                notion.add_record_to_notion_database(share_url)
             
         except:
             print("Got error")
--- settings.py
+++ settings.py
@@ -25,6 +25,7 @@ class Settings:
         self.server_name = self.default_get(data, 'server_name', "")
         self.server_port = self.default_get(data, 'server_port', 0)
         self.server_share = self.default_get(data, 'server_share', True)
+        self.reg_notion = self.default_get(data, 'reg_notion', False)
         self.output_image_format = self.default_get(data, 'output_image_format', 'png')
         self.output_video_format = self.default_get(data, 'output_video_format', 'mp4')
         self.output_video_codec = self.default_get(data, 'output_video_codec', 'libx264')
@@ -46,6 +47,7 @@ class Settings:
             'server_name': self.server_name,
             'server_port': self.server_port,
             'server_share': self.server_share,
+            'reg_notion': self.reg_notion,
             'output_image_format' : self.output_image_format,
             'output_video_format' : self.output_video_format,
             'output_video_codec' : self.output_video_codec,
