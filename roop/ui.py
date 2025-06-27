6f3d311eef6f725e1fb6ea14babf005f6f6ab7e7
stephen <6983299@gmail.com>
2025-06-27 16:05:27 +0800

    trythen

--- roop/ui.py
+++ roop/ui.py
@@ -476,7 +476,7 @@ def run():
 
             bt_preview_mask.click(fn=on_preview_mask, inputs=[preview_frame_num, bt_destfiles, clip_text], outputs=[maskpreview]) 
 
-            start_event = bt_start.click(fn=start_swap, 
+            bt_start.click(fn=start_swap, 
                 inputs=[selected_enhancer, selected_face_detection, roop.globals.keep_fps, roop.globals.keep_frames,
                          roop.globals.skip_audio, max_face_distance, blend_ratio, bt_destfiles, chk_useclip, clip_text,video_swapping_method, hf_token, 
                          hidden_input, hidden_ip, hidden_finger1, hidden_finger2],
@@ -498,7 +498,8 @@ def run():
                     """
                 )
             
-            bt_stop.click(fn=stop_swap, cancels=[start_event])
+            # bt_stop.click(fn=stop_swap, cancels=[start_event])
+            bt_stop.click(fn=stop_swap)
             
             bt_refresh_preview.click(fn=on_preview_frame_changed, inputs=previewinputs, outputs=[previewimage])            
             fake_preview.change(fn=on_preview_frame_changed, inputs=previewinputs, outputs=[previewimage])
