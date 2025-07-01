0e7a21a5f8b7e06600000a1a80d9fc02db3285fd
stephen <6983299@gmail.com>
2025-07-01 14:41:48 +0800

    yield gr.Button.update(variant="primary"), None, None

--- roop/ui.py
+++ roop/ui.py
@@ -994,7 +994,7 @@ def start_swap(enhancer, detection, keep_fps, keep_frames, skip_audio, face_dist
     #     yield gr.Button.update(variant="primary"), gr.Files.update(value=outfiles), gr.Image.update(value=outfiles[0])
     # else:
     #     yield gr.Button.update(variant="primary"), None, None
-
+    yield gr.Button.update(variant="primary"), None, None
 
 def stop_swap():
     roop.globals.processing = False
