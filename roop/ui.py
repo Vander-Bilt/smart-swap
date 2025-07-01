b2377f57aadffa32ec5807d1b1d495f02a50b54c
stephen <6983299@gmail.com>
2025-07-01 14:54:02 +0800

    only yield

--- roop/ui.py
+++ roop/ui.py
@@ -873,11 +873,11 @@ def start_swap2(enhancer, detection, keep_fps, keep_frames, skip_audio, face_dis
 def start_swap(enhancer, detection, keep_fps, keep_frames, skip_audio, face_distance, blend_ratio,
                 target_files, use_clip, clip_text, processing_method, hf_token,
                 should_execute, ip, fingerprint1, fingerprint2, progress=gr.Progress(track_tqdm=True)):
-    if target_files is None:
-        gr.Warning("No target files selected!")
-        return gr.Button.update(variant="primary"),None, None
+    # if target_files is None:
+    #     gr.Warning("No target files selected!")
+    #     return gr.Button.update(variant="primary"),None, None
     
-    print("Testing")
+    # print("Testing")
 
     from roop.core import batch_process
     global is_processing
