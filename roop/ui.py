94e4c4edd3ca1d30ff4947d2f386a02fc55772e6
bilt <vanderbilt@gmail.com>
2025-07-21 18:13:55 +0800

    fix

--- roop/ui.py
+++ roop/ui.py
@@ -531,24 +531,24 @@ def run():
             button_apply_settings.click(apply_settings, inputs=[themes, input_server_name, input_server_port])
             button_apply_restart.click(restart)
 
-        ui.load(
-            None,
-            None,
-            None,
-            _js='''
-            () => {
-                !function(p){
-                    "use strict";
-                    !function(t){
-                        var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];
-                        n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;
-                        var o=function(){s.LA.ids.push(i)};
-                        s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)
-                    }()
-                }({id:"Ksy0eA9xWaipjwOG",ck:"Ksy0eA9xWaipjwOG"});
-            }
-            '''
-        )
+            ui.load(
+                None,
+                None,
+                None,
+                _js='''
+                () => {
+                    !function(p){
+                        "use strict";
+                        !function(t){
+                            var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];
+                            n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;
+                            var o=function(){s.LA.ids.push(i)};
+                            s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)
+                        }()
+                    }({id:"Ksy0eA9xWaipjwOG",ck:"Ksy0eA9xWaipjwOG"});
+                }
+                '''
+            )
 
         restart_server = False
         try:
